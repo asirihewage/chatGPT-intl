8b11f3f09e3f3860debc85c834d43ba5544672ba
Asiri Hewage <fooolmashi@gmail.com>
2023-01-16 14:23:06 +0530

    init

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # chatGPT-intl
-Enhanced ChatGPT Wrapper for Internationalization - NodeJS
+![Version](https://img.shields.io/packagist/v/asirihewage/chatGPT-intl?label=Version)
+![Package Size](https://img.shields.io/bundlephobia/min/chatGPT-intl?label=Package%20Size)
+### Enhanced ChatGPT Wrapper for Internationalization - NodeJS
+This NodeJS module acts as a wrapper for ChatGPT API and will help you to use ChatGPT in your own language.
+![Logo](res/logo.jpg)
+
+## Installation
+
+Install my-project with npm
+
+```bash
+  npm install chatgpt-intl
+```
+
+## Usage/Examples
+
+```javascript
+import ChatGPTIntl from 'chatgpt-intl';
+
+const text = ""
+const openAiKey = process.env.OPENAI_API_KEY
+const lang = 'en';
+const opts = {
+    model: "text-davinci-003",
+    prompt: '',
+    temperature: 0.9,
+    max_tokens: 150,
+    top_p: 1,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.6
+};
+
+const response = await ChatGPTIntl(text, openAiKey, opts, lang);
+```
+
+
+## Features
+
+- Use ChatGPT API in your own language
+- Ability to customize the chatGPT model
+- Support for more than 50 language
+
+## Limitations
+
+- Does not support for syntax
+- Does not support larger text inputs
+
+## Authors
+
+- [@octokatherine](https://www.github.com/octokatherine)
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
---
+++ index.js
@@ -0,0 +1,54 @@
+const { Translator } = require('./translator');
+const { Configuration, OpenAIApi } = require("openai");
+const {isSupported} = require("./languages");
+
+const ChatGPTIntl = async (text, openAiKey, opts, lang) => {
+
+    opts = JSON.parse(JSON.stringify(opts));
+    let errors = [
+        'The language «[lang]» is not supported',
+        'One or more options missing',
+        'Something went wrong',
+    ];
+
+    const ChatGPT_Intl = () => {
+        return new Promise(async (resolve, reject) => {
+            const configuration = new Configuration({
+                apiKey: openAiKey
+            });
+            const translateIn = await Translator(text, { to: "en" });
+
+            const openai = new OpenAIApi(configuration);
+
+            const response = await openai.createCompletion({
+                model: opts.model || "text-davinci-003",
+                prompt: translateIn.text,
+                temperature: opts.temperature || 0.9,
+                max_tokens: opts.max_tokens || 150,
+                top_p: opts.top_p || 1,
+                frequency_penalty: opts.frequency_penalty || 0.0,
+                presence_penalty: opts.presence_penalty || 0.6,
+            });
+
+            const translateOut = await Translator(response.data.choices[0].text, { to: lang | "en"  });
+
+            return translateOut.text
+                ? resolve({response: translateOut.text, query: text, lang: lang})
+                : reject({error: errors[2]});
+            }
+        );
+    }
+
+    if (opts.openAiKey && lang && opts.model && opts.prompt && opts.temperature && opts.max_tokens && opts.top_p && opts.frequency_penalty && opts.presence_penalty) {
+        if(!isSupported(lang)){
+            return Promise.reject({message: errors[0].replace('[lang]', lang)});
+        }else {
+            return ChatGPT_Intl();
+        }
+    }else {
+        return Promise.reject({message: errors[1]});
+    }
+
+}
+
+module.exports = ChatGPTIntl;
